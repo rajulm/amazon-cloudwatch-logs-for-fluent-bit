52d5402eb5104a9256e40d66b70ed43b35c0389b
Rajul Mittal <rajulm@amazon.com>
2019-12-05 15:58:48 -0800

    Changing Slice to SliceStable

--- cloudwatch/cloudwatch.go
+++ cloudwatch/cloudwatch.go
@@ -448,7 +448,7 @@ func (output *OutputPlugin) putLogEvents(stream *logStream) error {
 
 	logrus.Infof("[rajulm kajeeth] Log events before sorting are %s\n", stream.logEvents)
 	// Log events in a single PutLogEvents request must be in chronological order.
-	sort.Slice(stream.logEvents, func(i, j int) bool {
+	sort.SliceStable(stream.logEvents, func(i, j int) bool {
 		return aws.Int64Value(stream.logEvents[i].Timestamp) < aws.Int64Value(stream.logEvents[j].Timestamp)
 	})
 	logrus.Infof("[rajulm kajeeth] Log events after sorting are %s\n", stream.logEvents)
